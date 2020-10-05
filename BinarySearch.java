27edf8f2f9d5f3a4faa808e8273088456c8ed060
Chris Toxiadis <chris.it185288@gmail.com>
2020-10-05 22:15:40 +0300

    Initial commit

---
+++ BinarySearch.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class Search{
+
+  public static void main(String[] args){
+    Scanner scan = new Scanner(System.in);
+    int number  = scan.nextInt();
+    int[] array = new int[20];
+    makeArray(array,20);
+    int position = search(array,number,0,array.length -1,0);
+    System.out.println(position < 0 ? "false" : position);
+    scan.close();
+  }
+
+  public static int search(int[] array, int number, int start, int end, int mid){
+    while(start <= end){
+      mid = (start + end) / 2;
+      if(array[mid] == number)
+        return mid;
+      else if(array[mid] < number)
+        start = mid + 1;
+      else
+        end = mid - 1;
+    }
+    return -1;
+  }
+
+  private static void makeArray(int[] array, int size){
+    Random rand = new Random();
+    array[0] = rand.nextInt(10);
+    for(int i = 1; i < size; i++)
+      do
+        array[i] = rand.nextInt(500);
+      while (array[i] < array[i-1]);
+  }
+
+}
